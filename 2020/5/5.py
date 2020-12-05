9e28ebfa8ebaf669581e3c82319d55155d9c71ea
JohanKJIP <JohanKJIP@gmail.com>
2020-12-05 22:02:42 +0100

    More compact day 5 solution

--- 2020/5/5.py
+++ 2020/5/5.py
@@ -1,7 +1,5 @@
 def get_seat_id(tag: str) -> int:
-    row = int(tag[0:7].replace('B', '1').replace('F', '0'), 2)
-    col = int(tag[7:10].replace('R', '1').replace('L', '0'), 2)
-    return row * 8 + col
+    return int(tag[:7], 2) * 8 + int(tag[7:], 2)
 
 def find_empty_seat(ids: [int]) -> int:
     ids = sorted(ids)
@@ -9,7 +7,7 @@ def find_empty_seat(ids: [int]) -> int:
 
 if __name__ == "__main__":
     with open('input.in', 'r') as pass_file:
-        passes = pass_file.read().split('\n')[:-1]
+        passes = pass_file.read().replace('B', '1').replace('F', '0').replace('R', '1').replace('L', '0').split('\n')[:-1]
         ids = [get_seat_id(tag) for tag in passes]
         print(max(ids))
         print(find_empty_seat(ids))
